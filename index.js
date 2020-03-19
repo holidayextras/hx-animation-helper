347af77e4147840b1dbffd4d93d56b43e21b196e
LouisPryer <louis.pryer@holidayextras.com>
2020-03-19 11:31:30 +0000

    Remove jQuery dependency and fix tabs

--- index.js
+++ index.js
@@ -1,26 +1,37 @@
 'use strict';
 function hxAnimationHelper (target, animationClasses) {
   if (!target || !animationClasses) return null
-  var transitionEvent = function () {
-    var t, el = document.createElement("fakeelement")
 
-	  var transitions = {
+  var element = document.querySelector(target) || null;
+  if (!element) return null
+
+  var transitionEvent = function () {
+    var t, el = document.createElement("fakeElement")
+
+    var transitions = {
       "animation": "animationend",
       "OAnimation": "oAnimationEnd",
       "MozAnimation": "animationend",
-      "WebkitAnimation": "webkitAnimationEnd"
+      "WebkitAnimation": "webkitAnimationEnd",
+      "transition":"transitionend",
+      "OTransition":"oTransitionEnd",
+      "MozTransition":"transitionend",
+      "WebkitTransition":"webkitTransitionEnd"
     }
 
     for (t in transitions){
- 	    if (el.style[t] !== undefined){
-  	    return transitions[t];
+      if (el.style[t] !== undefined){
+        return transitions[t];
       }
     }
   }
-  target.toggleClass(animationClasses)
-  target.one(transitionEvent(), () => {
-    target.toggleClass(animationClasses)
-  })
+
+  var waitForAnimations = transitionEvent();
+  element.toggle(animationClasses);
+
+  waitForAnimations && e.addEventListener(waitForAnimations, function() {
+    element.toggle(animationClasses);
+  });
 }
 
 module.exports = hxAnimationHelper
